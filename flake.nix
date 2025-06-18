afb1132421278dbef0dbf32d1e1c184b1bd01a4f
mow44 <mow44@proton.me>
2025-06-18 12:21:09 +0300

    Fix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,6 @@
 
   outputs =
     {
-      self,
       flake-utils,
       nixpkgs,
       ...
@@ -30,15 +29,26 @@
                 uxn
               ];
 
-              src = self;
+              src = pkgs.fetchFromSourcehut {
+                owner = "~rabbits";
+                repo = "noodle";
+                rev = "403f9ab8c68d91aae188d9546be76a8cf803ccc6";
+                hash = "sha256-rBMhDzaCqSVZCssddDA8GOeZAR893u7pBa0VP3cwEW4="; # lib.fakeHash;
+              };
+
+              patchPhase = ''
+                substituteInPlace src/manifest.tal \
+                --replace ".theme" "/home/a/.config/uxn/theme"
+              '';
 
               buildPhase = ''
-                uxnasm noodle.tal noodle.rom
+                uxnasm src/noodle.tal noodle.rom
               '';
 
               installPhase = ''
                 mkdir -p $out/bin
                 cp noodle.rom $out/bin
+                cp src/manifest.tal $out/bin
               '';
             };
         };
